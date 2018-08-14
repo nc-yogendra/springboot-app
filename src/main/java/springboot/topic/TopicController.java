10cf1f1bd15e2f6520207d35cd58301e25c4fa2e
Yogendra Maurya <myogendra15@gmail.com>
2018-08-14 16:50:51 +0530

    Dependency injection and Service providing

--- src/main/java/springboot/topic/TopicController.java
+++ src/main/java/springboot/topic/TopicController.java
@@ -1,5 +1,6 @@
 package springboot.topic;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,12 +10,11 @@ import java.util.List;
 @RestController
 public class TopicController {
 
+    @Autowired
+    private TopicService topicService;
+
     @RequestMapping("/topics")
     public List<Topic> getAllTopics(){
-        return Arrays.asList(
-                new Topic("Spring","Spring Framework","Spring Framework Description"),
-                new Topic("Java","Core Java","Core Java Description"),
-                new Topic("DBMS","Database Management System","DBMS Description")
-        );
+        return topicService.getAllTopics();
     }
 }
---
+++ src/main/java/springboot/topic/TopicService.java
@@ -0,0 +1,18 @@
+package springboot.topic;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class TopicService {
+    private List<Topic> topics = Arrays.asList(new Topic("Spring","Spring Framework","Spring Framework Description"),
+            new Topic("Java","Core Java","Core Java Description"),
+            new Topic("DBMS","Database Management System","DBMS Description"));
+
+    public List<Topic> getAllTopics(){
+        return topics;
+    }
+}
