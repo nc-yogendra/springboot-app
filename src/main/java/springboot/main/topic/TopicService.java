7f49c22ec022ab2fae60a0e28976aa5083a14699
Yogendra Maurya <myogendra15@gmail.com>
2018-08-14 19:15:55 +0530

    Adding new Topic to List using POST method

--- src/main/java/springboot/main/topic/TopicService.java
+++ src/main/java/springboot/main/topic/TopicService.java
@@ -2,14 +2,15 @@ package springboot.main.topic;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class TopicService {
-    private List<Topic> topics = Arrays.asList(new Topic("Spring","Spring Framework","Spring Framework Description"),
+    private List<Topic> topics = new ArrayList<>( Arrays.asList(new Topic("Spring","Spring Framework","Spring Framework Description"),
             new Topic("Java","Core Java","Core Java Description"),
-            new Topic("DBMS","Database Management System","DBMS Description"));
+            new Topic("DBMS","Database Management System","DBMS Description")));
 
     public List<Topic> getAllTopics(){
         return topics;
@@ -17,5 +18,9 @@ public class TopicService {
 
     public Topic getTopic(String id){
         return topics.stream().filter(t -> t.getId().equalsIgnoreCase(id)).findFirst().get();
+    }
+
+    public void addtoList(Topic topic){
+        topics.add(topic);
     }
 }
