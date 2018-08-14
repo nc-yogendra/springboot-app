316b2cded1b8c8e2fb52b43da2a6508650be015b
Yogendra Maurya <myogendra15@gmail.com>
2018-08-14 16:03:15 +0530

    Returning a List

--- src/main/java/springboot/topic/TopicController.java
+++ src/main/java/springboot/topic/TopicController.java
@@ -3,11 +3,18 @@ package springboot.topic;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RestController
 public class TopicController {
 
-    @RequestMapping("/topic")
-    public String getAllTopics(){
-        return "All Topics";
+    @RequestMapping("/topics")
+    public List<Topic> getAllTopics(){
+        return Arrays.asList(
+                new Topic("Spring","Spring Framework","Spring Framework Description"),
+                new Topic("Java","Core Java","Core Java Description"),
+                new Topic("DBMS","Database Management System","DBMS Description")
+        );
     }
 }
