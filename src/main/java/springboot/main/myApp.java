5c5879e6f78189a556d363a0a9e7db217717e196
Yogendra Maurya <myogendra15@gmail.com>
2018-08-14 12:49:09 +0530

    Spring Boot app initial Commit

---
+++ src/main/java/springboot/main/myApp.java
@@ -0,0 +1,11 @@
+package springboot.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class myApp {
+    public static void main(String args[]){
+        SpringApplication.run(myApp.class, args);
+    }
+}
